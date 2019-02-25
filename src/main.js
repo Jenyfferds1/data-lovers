7085a361d8fde1cf12e0d5de61a03cbcc800acfc
MarianaPrimante <maprimante@gmail.com>
2019-02-25 15:32:02 -0300

    inicio

---
+++ src/main.js
@@ -0,0 +1,85 @@
+function mostraOcultar(e) {
+    let tabSeleccionado = e.target.dataset.tabSeleccionado;
+    let brasil = document.getElementById("brasil");
+    let chile = document.getElementById("chile");
+    let mexico = document.getElementById("mexico");
+    let peru = document.getElementById("peru");
+    let am = document.getElementById("am");
+
+    if (tabSeleccionado === "tabAm") {
+        am.style.display = 'block';
+        chile.style.display = 'none';
+        mexico.style.display = 'none';
+        peru.style.display = 'none';
+        brasil.style.display = 'none';
+
+
+    }
+    if (tabSeleccionado === "tabBrasil") {
+        am.style.display = 'none';
+        chile.style.display = 'none';
+        mexico.style.display = 'none';
+        peru.style.display = 'none';
+        brasil.style.display = 'block';
+
+    }
+    if (tabSeleccionado === "tabChile") {
+        am.style.display = 'none';
+        brasil.style.display = 'none';
+        mexico.style.display = 'none';
+        peru.style.display = 'none';
+        chile.style.display = 'block';
+
+
+    }
+    if (tabSeleccionado === "tabMexico") {
+        am.style.display = 'none';
+        chile.style.display = 'none';
+        brasil.style.display = 'none';
+        peru.style.display = 'none';
+        mexico.style.display = 'block';
+
+    }
+    if (tabSeleccionado === "tabPeru") {
+        am.style.display = 'none';
+        chile.style.display = 'none';
+        mexico.style.display = 'none';
+        brasil.style.display = 'none';
+        peru.style.display = 'block';
+    }
+};
+
+
+function carregarPag() {
+    let brasil = document.getElementById("brasil");
+    let chile = document.getElementById("chile");
+    let mexico = document.getElementById("mexico");
+    let peru = document.getElementById("peru");
+    let am = document.getElementById("am");
+    chile.style.display = 'none';
+    mexico.style.display = 'none';
+    brasil.style.display = 'none';
+    peru.style.display = 'none';
+    am.style.display = 'block';
+    let elementosTab = document.getElementsByClassName("tab");
+
+    for (let elementos of elementosTab) {
+        elementos.addEventListener('click', mostraOcultar)
+    }
+}
+carregarPag();
+
+
+let pagBra = document.getElementById("tabbra");
+pagBra.addEventListener('click', carregaPagBra)
+
+function carregaPagBra() {
+    let paisSede = pagBra.value;
+    let bra = document.getElementById("conteudobrasil");
+    for (sede in WORLDBANK[paisSede]) {
+        for (i in WORLDBANK[paisSede][sede]["indicators"]) {
+
+        }
+    }
+
+}
